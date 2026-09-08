44d4d946bc96a396c739d23a2a6f6739ccf8bc86
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-09-08 15:33:46 +0800

    Update libc_shim.h

--- source/libc_shim.h
+++ source/libc_shim.h
@@ -204,4 +204,7 @@ size_t __strlcpy_chk_fake(char *dst, const char *src, size_t dstsize, size_t dst
 size_t __strlcat_chk_fake(char *dst, const char *src, size_t dstsize, size_t dstlen);
 char *__strncpy_chk_fake(char *dst, const char *src, size_t n, size_t dstlen);
 
+long lseek64_fake(int fd, long offset, int whence);
+int poll_fake(void *fds, unsigned long nfds, int timeout);
+
 #endif
