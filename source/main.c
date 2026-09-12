02df0a2807b30c4e2a9bc6a6a51b57c9b316d10f
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-09-12 16:28:59 +0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -320,8 +320,8 @@ static void dump_sdl_thread_state(void) {
   ThreadContext ctx;
   if (R_SUCCEEDED(svcGetThreadContext3(&ctx, s_sdl_thread.handle))) {
     debugPrintf("[heartbeat] sdl_thread pc=%p lr=%p sp=%p x0=%p x8=%p\n",
-                (void *)ctx.pc, (void *)ctx.lr, (void *)ctx.sp,
-                (void *)ctx.x0, (void *)ctx.x8);
+                (void *)ctx.pc.x, (void *)ctx.lr.x, (void *)ctx.sp.x,
+                (void *)ctx.cpu_gprs[0].x, (void *)ctx.cpu_gprs[8].x);
   } else {
     debugPrintf("[heartbeat] svcGetThreadContext3 failed\n");
   }
